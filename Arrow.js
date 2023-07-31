bee3b3d321f4ccb3392c58b076f07a01657ed4f6
utpal-sql <ut.chakraborty@gmail.com>
2023-08-01 00:23:15 +0530

    CommitES6

---
+++ Arrow.js
@@ -0,0 +1,17 @@
+
+var a = 5;
+var b = 5;
+console.log(a + b);
+
+//ES5
+//function declaration/definition
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(sum(10, 5)); //function call/function invocation
+
+
+//ES6 => arrow function 
+const sum = (num1, num2) =>  num1 + num2;
+
